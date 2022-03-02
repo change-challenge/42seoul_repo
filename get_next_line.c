e9ba2cf4f2256cc5dd6123d1ef0fd6abd458674f
change-challenge <dog941128@hotmail.com>
2022-03-02 15:45:16 +0900

    0302

--- get_next_line.c
+++ get_next_line.c
@@ -6,52 +6,79 @@
 /*   By: hchang <hchang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 17:20:02 by hchang            #+#    #+#             */
-/*   Updated: 2022/03/01 21:57:33 by hchang           ###   ########.fr       */
+/*   Updated: 2022/03/02 15:42:19 by hchang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 // 삭제할 헤더
 #include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <stdlib.h>
+
+int	check_str_null_eof(char *tmp)
+{
+	while (*tmp++)
+	{
+		if (*tmp == '\0' || *tmp == '\n')
+			return (ERROR);
+	}
+	return (SUCCESS);
+}
+
+int	lstadd_front(t_list *gnl_list, char *tmp)
+{
+	int	len;
+
+	while (*tmp == '\0' || *tmp == '\n')
+	{
+		len++;
+		tmp++;
+	}
+	while (gnl_list->next == NULL)
+	{
+		gnl_list->str = (char*)malloc(sizeof(char) * (len + 1));
+		gnl_list = gnl_list->next;
+	}
+	return (len);
+}
 
 char *get_next_line(int fd)
 {
-	char *result;
-	
-	result = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	return (result);
+	static char	*back_up;
+	t_list		*gnl_list;
+	size_t		len;
+	char		*tmp;
+	char 		*result;
+
+
+	// 오히려 마지막에 result malloc과 input 
+
 }
+
+
+
+
+
+
+
+
+
+
+
 
 int main() 
 { 
-	int fd;
-	int i;
-	int j;
+	char 	str[BUFFER_SIZE]; 
+	int		fd;
+	int		read_len;
+
 	
-	char *line = 0;
-	char *lineadress[66]; 
-	j = 1;
-	
-	printf("\n==========================================\n");
-	printf("========= TEST 2 : Empty Lines ===========\n"); 
-	printf("==========================================\n\n"); 
-	
-	if (!(fd = open("/a.txt", O_RDONLY))) 
-	{
-		printf("\nError in open\n");
-		return (0);
-	} 
-	
-	while ((line = get_next_line(fd)) > 0)
-	{
-		printf("|%s\n", line);
-		free(line);
-		j++;
-	} 
-	printf("|%s\n", line);
-	free(line);
+	fd = open("./a.txt", O_RDONLY);
+	// read_len = read(fd, str, BUFFER_SIZE);
+	// str[BUFFER_SIZE] = '\0';
+	// printf("|%s|\n", str);
+
+
+	printf("|%d|\n", read_len);
 	close(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,18 @@
 /*   By: hchang <hchang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 21:20:19 by hchang            #+#    #+#             */
-/*   Updated: 2022/03/01 21:59:43 by hchang           ###   ########.fr       */
+/*   Updated: 2022/03/02 15:08:45 by hchang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-
+#  define ERROR 0
+#  define SUCCESS 1
 #ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 #endif 
@@ -21,7 +25,7 @@
 typedef struct s_list
 {
 	char			*str;
-	struct t_list	*next;
+	struct s_list	*next;
 }	t_list;
 
 char *get_next_line(int fd);
